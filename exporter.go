30de59845bd4741990db3dce8b6afbe66ea9ab30
Jean-Francois Chevrette <jfchevrette@gmail.com>
2019-12-03 16:50:08 -0500

    add comments to make go-lint happy

--- exporter.go
+++ exporter.go
@@ -7,6 +7,7 @@ import (
 	"github.com/prometheus/client_golang/prometheus"
 )
 
+// ExporterMetrics defines an instance of the exporter metrics
 type ExporterMetrics struct {
 	sess *session.Session
 
@@ -19,6 +20,7 @@ type ExporterMetrics struct {
 	mutex *sync.Mutex
 }
 
+// NewExporterMetrics creates a new exporter metrics instance
 func NewExporterMetrics(sess *session.Session) *ExporterMetrics {
 	return &ExporterMetrics{
 		sess: sess,
@@ -38,22 +40,26 @@ func NewExporterMetrics(sess *session.Session) *ExporterMetrics {
 	}
 }
 
+// Describe is used by the Prometheus client to return a description of the metrics
 func (e *ExporterMetrics) Describe(ch chan<- *prometheus.Desc) {
 	ch <- e.APIRequests
 	ch <- e.APIErrors
 }
 
+// Collect is used by the Prometheus client to collect and return the metrics values
 func (e *ExporterMetrics) Collect(ch chan<- prometheus.Metric) {
 	ch <- prometheus.MustNewConstMetric(e.APIRequests, prometheus.CounterValue, e.APIRequestsCount)
 	ch <- prometheus.MustNewConstMetric(e.APIErrors, prometheus.CounterValue, e.APIErrorsCount)
 }
 
+// IncrementRequests increments the API requests counter
 func (e *ExporterMetrics) IncrementRequests() {
 	e.mutex.Lock()
 	e.APIRequestsCount++
 	e.mutex.Unlock()
 }
 
+// IncrementErrors increments the API requests error counter
 func (e *ExporterMetrics) IncrementErrors() {
 	e.mutex.Lock()
 	e.APIErrorsCount++
