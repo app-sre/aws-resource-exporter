6581f6204ef6c12c06467eafe13e5a9d4794e01c
Marius Schulz <2656597+mrWinston@users.noreply.github.com>
2022-08-30 15:57:53 +0200

    concurrent r53 metric collection (#47)

--- route53.go
+++ route53.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	"context"
+	"fmt"
+	"math"
+	"sync"
 	"time"
 
 	"github.com/aws/aws-sdk-go/aws"
@@ -12,54 +15,95 @@ import (
 	"github.com/prometheus/client_golang/prometheus"
 )
 
+const (
+	maxRetries = 5
+)
+
 type Route53Exporter struct {
 	sess                      *session.Session
 	RecordsPerHostedZoneQuota *prometheus.Desc
 	RecordsPerHostedZoneUsage *prometheus.Desc
+	LastUpdateTime            *prometheus.Desc
+	Cancel                    context.CancelFunc
 
-	logger  log.Logger
-	timeout time.Duration
+	cachedMetrics []prometheus.Metric
+	metricsMutex  *sync.Mutex
+	logger        log.Logger
+	interval      time.Duration
+	timeout       time.Duration
 }
 
-func NewRoute53Exporter(sess *session.Session, logger log.Logger, timeout time.Duration) *Route53Exporter {
+func NewRoute53Exporter(sess *session.Session, logger log.Logger, interval time.Duration, timeout time.Duration) *Route53Exporter {
 
 	level.Info(logger).Log("msg", "Initializing Route53 exporter")
 
-	return &Route53Exporter{
+	exporter := &Route53Exporter{
 		sess:                      sess,
 		RecordsPerHostedZoneQuota: prometheus.NewDesc(prometheus.BuildFQName(namespace, "", "route53_recordsperhostedzone_quota"), "Quota for maximum number of records in a Route53 hosted zone", []string{"hostedzoneid", "hostedzonename"}, nil),
 		RecordsPerHostedZoneUsage: prometheus.NewDesc(prometheus.BuildFQName(namespace, "", "route53_recordsperhostedzone_total"), "Number of Resource records", []string{"hostedzoneid", "hostedzonename"}, nil),
+		LastUpdateTime:            prometheus.NewDesc(prometheus.BuildFQName(namespace, "", "route53_last_updated_timestamp_seconds"), "Last time, the route53 metrics were sucessfully updated", []string{}, nil),
+		cachedMetrics:             []prometheus.Metric{},
+		metricsMutex:              &sync.Mutex{},
 		logger:                    logger,
+		interval:                  interval,
 		timeout:                   timeout,
+	}
+	return exporter
+}
+
+func (e *Route53Exporter) CollectLoop() {
+	route53Svc := route53.New(e.sess)
+
+	for {
+		ctx, ctxCancelFunc := context.WithTimeout(context.Background(), e.timeout)
+		e.Cancel = ctxCancelFunc
+		level.Info(e.logger).Log("msg", "Updating Route53 metrics...")
+
+		hostedZones, err := getAllHostedZones(route53Svc, ctx)
+		level.Info(e.logger).Log("msg", "Got all zones")
+		if err != nil {
+			level.Error(e.logger).Log("msg", "Could not retrieve the list of hosted zones", "error", err.Error())
+			exporterMetrics.IncrementErrors()
+		}
+		tmpMetrics := []prometheus.Metric{}
+
+		for i, hostedZone := range hostedZones {
+			hostedZoneLimitOut, err := GetHostedZoneLimitWithBackoff(route53Svc, ctx, hostedZone.Id, maxRetries)
+
+			if err != nil {
+				level.Error(e.logger).Log("msg", "Could not get Quota for hosted zone", "hostedZoneId", hostedZone.Id, "hostedZoneName", hostedZone.Name, "error", err.Error())
+				exporterMetrics.IncrementErrors()
+				continue
+			}
+			level.Info(e.logger).Log("msg", "R53 Gathered", "num", i, "total", len(hostedZones))
+			tmpMetrics = append(tmpMetrics,
+				prometheus.MustNewConstMetric(e.RecordsPerHostedZoneQuota, prometheus.GaugeValue, float64(*hostedZoneLimitOut.Limit.Value), *hostedZone.Id, *hostedZone.Name),
+				prometheus.MustNewConstMetric(e.RecordsPerHostedZoneUsage, prometheus.GaugeValue, float64(*hostedZoneLimitOut.Count), *hostedZone.Id, *hostedZone.Name),
+			)
+		}
+		e.metricsMutex.Lock()
+		e.cachedMetrics = append(tmpMetrics, prometheus.MustNewConstMetric(e.LastUpdateTime, prometheus.GaugeValue, float64(time.Now().Unix())))
+		e.metricsMutex.Unlock()
+		level.Info(e.logger).Log("msg", "Route53 metrics Updated")
+
+		ctxCancelFunc()
+
+		time.Sleep(e.interval)
 	}
 }
 
 func (e *Route53Exporter) Collect(ch chan<- prometheus.Metric) {
-	ctx, ctxCancel := context.WithTimeout(context.Background(), e.timeout)
-	defer ctxCancel()
-	route53Svc := route53.New(e.sess)
-
-	hostedZones, err := getAllHostedZones(route53Svc, ctx)
-	if err != nil {
-		level.Error(e.logger).Log("msg", "Could not retrieve the list of hosted zones", "error", err.Error())
-		exporterMetrics.IncrementErrors()
+	e.metricsMutex.Lock()
+	defer e.metricsMutex.Unlock()
+	for _, metric := range e.cachedMetrics {
+		ch <- metric
 	}
+}
 
-	for _, hostedZone := range hostedZones {
-		hostedZoneLimitOut, err := route53Svc.GetHostedZoneLimitWithContext(ctx, &route53.GetHostedZoneLimitInput{
-			HostedZoneId: hostedZone.Id,
-			Type:         aws.String(route53.HostedZoneLimitTypeMaxRrsetsByZone),
-		})
-
-		if err != nil {
-			level.Error(e.logger).Log("msg", "Could not get Quota for hosted zone", "hostedZoneId", hostedZone.Id, "hostedZoneName", hostedZone.Name, "error", err.Error())
-			exporterMetrics.IncrementErrors()
-			continue
-		}
-
-		ch <- prometheus.MustNewConstMetric(e.RecordsPerHostedZoneQuota, prometheus.GaugeValue, float64(*hostedZoneLimitOut.Limit.Value), *hostedZone.Id, *hostedZone.Name)
-		ch <- prometheus.MustNewConstMetric(e.RecordsPerHostedZoneUsage, prometheus.GaugeValue, float64(*hostedZoneLimitOut.Count), *hostedZone.Id, *hostedZone.Name)
-	}
+func (e *Route53Exporter) Describe(ch chan<- *prometheus.Desc) {
+	ch <- e.RecordsPerHostedZoneQuota
+	ch <- e.RecordsPerHostedZoneUsage
+	ch <- e.LastUpdateTime
 }
 
 func getAllHostedZones(client *route53.Route53, ctx context.Context) ([]*route53.HostedZone, error) {
@@ -67,14 +111,14 @@ func getAllHostedZones(client *route53.Route53, ctx context.Context) ([]*route53
 
 	listZonesInput := route53.ListHostedZonesInput{}
 
-	listZonesOut, err := client.ListHostedZonesWithContext(ctx, &listZonesInput)
+	listZonesOut, err := ListHostedZonesWithBackoff(client, ctx, &listZonesInput, maxRetries)
 	if err != nil {
 		return nil, err
 	}
 	result = append(result, listZonesOut.HostedZones...)
 
 	for *listZonesOut.IsTruncated {
-		listZonesInput.Marker = listZonesOut.Marker
+		listZonesInput.Marker = listZonesOut.NextMarker
 		listZonesOut, err = client.ListHostedZonesWithContext(ctx, &listZonesInput)
 		if err != nil {
 			return nil, err
@@ -85,7 +129,37 @@ func getAllHostedZones(client *route53.Route53, ctx context.Context) ([]*route53
 	return result, nil
 }
 
-func (e *Route53Exporter) Describe(ch chan<- *prometheus.Desc) {
-	ch <- e.RecordsPerHostedZoneQuota
-	ch <- e.RecordsPerHostedZoneUsage
+func ListHostedZonesWithBackoff(client *route53.Route53, ctx context.Context, input *route53.ListHostedZonesInput, maxTries int) (*route53.ListHostedZonesOutput, error) {
+	var listHostedZonesOut *route53.ListHostedZonesOutput
+	var err error
+
+	for i := 0; i < maxTries; i++ {
+		listHostedZonesOut, err = client.ListHostedZonesWithContext(ctx, input)
+		if err == nil {
+			return listHostedZonesOut, err
+		}
+		backOffSeconds := math.Pow(2, float64(i-1))
+		fmt.Printf("Backing off for %f seconds", backOffSeconds)
+		time.Sleep(time.Duration(backOffSeconds) * time.Second)
+	}
+	return nil, err
+}
+
+func GetHostedZoneLimitWithBackoff(client *route53.Route53, ctx context.Context, hostedZoneId *string, maxTries int) (*route53.GetHostedZoneLimitOutput, error) {
+	hostedZoneLimitInput := &route53.GetHostedZoneLimitInput{
+		HostedZoneId: hostedZoneId,
+		Type:         aws.String(route53.HostedZoneLimitTypeMaxRrsetsByZone),
+	}
+	var hostedZoneLimitOut *route53.GetHostedZoneLimitOutput
+	var err error
+
+	for i := 0; i < maxTries; i++ {
+		hostedZoneLimitOut, err = client.GetHostedZoneLimitWithContext(ctx, hostedZoneLimitInput)
+		if err == nil {
+			return hostedZoneLimitOut, err
+		}
+		backOffSeconds := math.Pow(2, float64(i-1))
+		time.Sleep(time.Duration(backOffSeconds) * time.Second)
+	}
+	return nil, err
 }
