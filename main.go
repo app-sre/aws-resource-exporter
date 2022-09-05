9e003ac714ae3a9282c0e001104c5d95383f667c
Erwan SEITE <eseite@talend.com>
2022-09-05 12:00:17 +0200

    use default credential management

--- main.go
+++ main.go
@@ -11,7 +11,7 @@ import (
 	"time"
 
 	"github.com/aws/aws-sdk-go/aws"
-	"github.com/aws/aws-sdk-go/aws/credentials"
+	//"github.com/aws/aws-sdk-go/aws/credentials"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/go-kit/kit/log/level"
 	"github.com/prometheus/common/promlog"
@@ -88,7 +88,8 @@ func loadExporterConfiguration(logger log.Logger, configFile string) (*Config, e
 	return &config, nil
 }
 
-func setupCollectors(logger log.Logger, configFile string, creds *credentials.Credentials) ([]prometheus.Collector, error) {
+//func setupCollectors(logger log.Logger, configFile string, creds *credentials.Credentials) ([]prometheus.Collector, error) {
+func setupCollectors(logger log.Logger, configFile string) ([]prometheus.Collector, error) {
 	var collectors []prometheus.Collector
 	config, err := loadExporterConfiguration(logger, configFile)
 	if err != nil {
@@ -102,7 +103,8 @@ func setupCollectors(logger log.Logger, configFile string, creds *credentials.Cr
 	level.Info(logger).Log("msg", "Will VPC metrics be gathered?", "vpc-enabled", config.VpcConfig.Enabled)
 	if config.VpcConfig.Enabled {
 		for _, region := range config.VpcConfig.Regions {
-			config := aws.NewConfig().WithCredentials(creds).WithRegion(region)
+			//config := aws.NewConfig().WithCredentials(creds).WithRegion(region)
+			config := aws.NewConfig().WithRegion(region)
 			sess := session.Must(session.NewSession(config))
 			vpcSessions = append(vpcSessions, sess)
 		}
@@ -112,7 +114,8 @@ func setupCollectors(logger log.Logger, configFile string, creds *credentials.Cr
 	var rdsSessions []*session.Session
 	if config.RdsConfig.Enabled {
 		for _, region := range config.RdsConfig.Regions {
-			config := aws.NewConfig().WithCredentials(creds).WithRegion(region)
+			//config := aws.NewConfig().WithCredentials(creds).WithRegion(region)
+			config := aws.NewConfig().WithRegion(region)
 			sess := session.Must(session.NewSession(config))
 			rdsSessions = append(rdsSessions, sess)
 		}
@@ -122,7 +125,8 @@ func setupCollectors(logger log.Logger, configFile string, creds *credentials.Cr
 	var ec2Sessions []*session.Session
 	if config.EC2Config.Enabled {
 		for _, region := range config.EC2Config.Regions {
-			config := aws.NewConfig().WithCredentials(creds).WithRegion(region)
+			//config := aws.NewConfig().WithCredentials(creds).WithRegion(region)
+			config := aws.NewConfig().WithRegion(region)
 			sess := session.Must(session.NewSession(config))
 			ec2Sessions = append(ec2Sessions, sess)
 		}
@@ -130,7 +134,8 @@ func setupCollectors(logger log.Logger, configFile string, creds *credentials.Cr
 	}
 	level.Info(logger).Log("msg", "Will Route53 metrics be gathered?", "route53-enabled", config.Route53Config.Enabled)
 	if config.Route53Config.Enabled {
-		awsConfig := aws.NewConfig().WithCredentials(creds).WithRegion(config.Route53Config.Region)
+		//awsConfig := aws.NewConfig().WithCredentials(creds).WithRegion(config.Route53Config.Region)
+		awsConfig := aws.NewConfig().WithRegion(config.Route53Config.Region)
 		sess := session.Must(session.NewSession(awsConfig))
 		r53Exporter := NewRoute53Exporter(sess, logger, config.Route53Config.Interval, config.Route53Config.Timeout)
 		collectors = append(collectors, r53Exporter)
@@ -151,11 +156,13 @@ func run() int {
 	level.Info(logger).Log("msg", "Starting"+namespace, "version", version.Info())
 	level.Info(logger).Log("msg", "Build context", version.BuildContext())
 
+	/*
 	creds := credentials.NewEnvCredentials()
 	if _, err := creds.Get(); err != nil {
 		level.Error(logger).Log("msg", "Could not get AWS credentials from env variables", "err", err)
 		return 1
 	}
+	*/
 
 	exporterMetrics = NewExporterMetrics()
 	var configFile string
@@ -164,7 +171,8 @@ func run() int {
 	} else {
 		configFile = CONFIG_FILE_PATH
 	}
-	cs, err := setupCollectors(logger, configFile, creds)
+	//cs, err := setupCollectors(logger, configFile, creds)
+	cs, err := setupCollectors(logger, configFile)
 	if err != nil {
 		level.Error(logger).Log("msg", "Could not load configuration file", "err", err)
 		return 1
