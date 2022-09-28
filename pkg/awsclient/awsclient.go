2a4e918d1c9b51455dc8f79f43a19429c8f9d267
Suzana Nesic <snesic@redhat.com>
2022-09-28 16:33:00 -0400

    adding tests for route53
    Signed-off-by: Suzana Nesic <snesic@redhat.com>

--- pkg/awsclient/awsclient.go
+++ pkg/awsclient/awsclient.go
@@ -6,7 +6,12 @@ import (
 	"context"
 
 	"github.com/aws/aws-sdk-go/aws/request"
+<<<<<<< HEAD
 	"github.com/aws/aws-sdk-go/service/rds"
+=======
+	"github.com/aws/aws-sdk-go/service/route53"
+	"github.com/aws/aws-sdk-go/service/route53/route53iface"
+>>>>>>> b04e4b2 (adding tests for route53)
 	"github.com/aws/aws-sdk-go/service/servicequotas"
 	"github.com/aws/aws-sdk-go/service/servicequotas/servicequotasiface"
 
@@ -33,12 +38,20 @@ type Client interface {
 
 	// Service Quota
 	GetServiceQuotaWithContext(ctx aws.Context, input *servicequotas.GetServiceQuotaInput, opts ...request.Option) (*servicequotas.GetServiceQuotaOutput, error)
+	RequestServiceQuotaIncrease(*servicequotas.RequestServiceQuotaIncreaseInput) (*servicequotas.RequestServiceQuotaIncreaseOutput, error)
+	ListRequestedServiceQuotaChangeHistory(*servicequotas.ListRequestedServiceQuotaChangeHistoryInput) (*servicequotas.ListRequestedServiceQuotaChangeHistoryOutput, error)
+	ListRequestedServiceQuotaChangeHistoryByQuota(*servicequotas.ListRequestedServiceQuotaChangeHistoryByQuotaInput) (*servicequotas.ListRequestedServiceQuotaChangeHistoryByQuotaOutput, error)
+
+	//route53
+	ListHostedZonesWithContext(ctx context.Context, input *route53.ListHostedZonesInput, opts ...request.Option) (*route53.ListHostedZonesOutput, error)
+	GetHostedZoneLimitWithContext(ctx context.Context, input *route53.GetHostedZoneLimitInput, opts ...request.Option) (*route53.GetHostedZoneLimitOutput, error)
 }
 
 type awsClient struct {
 	ec2Client           ec2iface.EC2API
 	rdsClient           rds.RDS
 	serviceQuotasClient servicequotasiface.ServiceQuotasAPI
+	route53Client       route53iface.Route53API
 }
 
 func (c *awsClient) DescribeTransitGatewaysWithContext(ctx aws.Context, input *ec2.DescribeTransitGatewaysInput, opts ...request.Option) (*ec2.DescribeTransitGatewaysOutput, error) {
@@ -115,10 +128,22 @@ func (c *awsClient) DescribeDBInstancesAll(ctx context.Context) ([]*rds.DBInstan
 	return instances, nil
 }
 
+func (c *awsClient) ListHostedZonesWithContext(ctx context.Context, input *route53.ListHostedZonesInput, opts ...request.Option) (*route53.ListHostedZonesOutput, error) {
+	return c.route53Client.ListHostedZonesWithContext(ctx, input, opts...)
+}
+
+func (c *awsClient) GetHostedZoneLimitWithContext(ctx context.Context, input *route53.GetHostedZoneLimitInput, opts ...request.Option) (*route53.GetHostedZoneLimitOutput, error) {
+	return c.route53Client.GetHostedZoneLimitWithContext(ctx, input, opts...)
+}
+
 func NewClientFromSession(sess *session.Session) Client {
 	return &awsClient{
 		ec2Client:           ec2.New(sess),
 		serviceQuotasClient: servicequotas.New(sess),
+<<<<<<< HEAD
 		rdsClient:           *rds.New(sess),
+=======
+		route53Client:       route53.New(sess),
+>>>>>>> b04e4b2 (adding tests for route53)
 	}
 }
---
+++ pkg/route53_test.go
@@ -0,0 +1,33 @@
+package pkg
+
+import (
+	"context"
+	"testing"
+
+	"github.com/app-sre/aws-resource-exporter/pkg/awsclient/mock"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/service/route53"
+	"github.com/golang/mock/gomock"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestGetHostedZoneLimitWithContext(t *testing.T) {
+	ctx := context.TODO()
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+	mockClient := mock.NewMockClient(ctrl)
+	mockClient.EXPECT().GetHostedZoneLimitWithContext(ctx,
+		createGetHostedZoneLimitInput(route53ServiceCode, hostedZonesQuotaCode)).Return(
+
+		&route53.GetHostedZoneLimitOutput{
+			Count: aws.Int64(12),
+			Limit: &route53.HostedZoneLimit{
+				Type:  aws.String("route53"),
+				Value: aws.Int64(10)}}, nil)
+
+	value, err := getQuotaValueWithContextRoute53(mockClient, route53ServiceCode, hostedZonesQuotaCode, ctx)
+	println("this is the value")
+	println(value)
+	assert.Nil(t, err)
+	assert.Equal(t, value, int64(10))
+}
