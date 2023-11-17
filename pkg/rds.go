60159366c2f3ae44f39c4a6ec02843472ebee960
Karl Fischer <kfischer@redhat.com>
2023-11-17 14:05:08 +0100

    add missing max connections

--- pkg/rds.go
+++ pkg/rds.go
@@ -123,6 +123,10 @@ var DBMaxConnections = map[string]map[string]int64{
 		"default.postgres13": 5000,
 		"default.postgres14": 5000,
 	},
+	"db.m5.16xlarge": map[string]int64{
+		"default":                 6000,
+		"default.aurora-mysql5.7": 6000,
+	},
 	"db.m5d.large": map[string]int64{
 		"default":            823,
 		"default.postgres11": 823,
